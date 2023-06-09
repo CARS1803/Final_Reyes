5a3a694f89d1d688d469eea98e3414278203843d
Reyes <Carlosreyes1803@gmail.com>
2023-06-09 15:12:20 -0600

    Se creeo la BD y se completaron los archivos del footer, header y navbar

---
+++ includes/navbar.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Registro de Personal</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Lista
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/controladores/ventas/detalle_ventas.php">Listado de empleados</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/ventas/buscar.php">Buscar Empleado</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Ingreso de Datos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/productos/index.php">Empleado Nuevo</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Areas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/clientes/index.php">Crear Area</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/crud_practica9/vistas/clientes/buscar.php">Buscar Area</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
